a44e4bde25a6dd294376cda99174b76bf8ad3924
Kayoshi-dev <maxime.jme@gmail.com>
2024-07-10 22:51:19 +0200

    Implement sortable thumbnails, used to reorder PDF pages

--- src/routes/pdf/+layout.svelte
+++ src/routes/pdf/+layout.svelte
@@ -1,10 +1,25 @@
 <script>
 	import { fileName } from '../../stores/FileStore';
+	import { ScanText } from 'lucide-svelte';
+	import { isTauri } from '@tauri-apps/api/core';
+
+	const OCRPdf = () => {
+		console.log('OCR PDF');
+	};
 </script>
 
-<nav class="bg-gray-50 dark:bg-gray-700">
-	<div class="max-w-screen-xl px-4 py-3">
-		<h4 class="font-bold">{$fileName}</h4>
+<nav class="flex justify-between items-center py-1 px-4 bg-gray-50 dark:bg-gray-700">
+	<h4 class="font-bold">{$fileName}</h4>
+
+	<div>
+		{#if isTauri()}
+			<button
+				on:click={OCRPdf}
+				class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-gray-200 h-10 px-4 py-2"
+			>
+				<ScanText />
+			</button>
+		{/if}
 	</div>
 </nav>
 
--- src/routes/pdf/+page.svelte
+++ src/routes/pdf/+page.svelte
@@ -3,18 +3,48 @@
 	import { processedFile } from '../../stores/FileStore';
 	import * as pdfjs from 'pdfjs-dist/legacy/build/pdf.mjs';
 	import 'pdfjs-dist/web/pdf_viewer.css';
+	import { goto } from '$app/navigation';
+	import { Sortable } from '@shopify/draggable';
 
 	const PDF_SCALE = 1.3;
 
-	let pages = Array.from({ length: $processedFile.numPages }, (_, i) => i + 1);
+	let pages: number[] = [];
 
 	let mainCanvas: HTMLCanvasElement;
 	let thumbnailsCanvas: HTMLCanvasElement[] = [];
 	let textLayerDiv: HTMLDivElement;
 
+	let thumbnailContainer: HTMLDivElement;
+
+	let sortable: any;
+
 	onMount(async () => {
-		loadThumbnails();
-		loadPage(1);
+		if ($processedFile) {
+			pages = Array.from({ length: $processedFile.numPages }, (_, i) => i + 1);
+			loadThumbnails();
+			loadPage(1);
+
+			sortable = new Sortable(thumbnailContainer, {
+				draggable: '.thumbnail-sub-container',
+
+				mirror: {
+					appendTo: thumbnailContainer,
+					constrainDimensions: true
+				}
+			});
+
+			sortable.on('sortable:stop', (event: { oldIndex: any; newIndex: any }) => {
+				const { oldIndex, newIndex } = event;
+
+				// Remove the page from the old index and insert it at the new index
+				const pageToMove = pages.splice(oldIndex, 1);
+				pages.splice(newIndex, 0, pageToMove[0]);
+
+				pages = [...pages];
+			});
+		} else {
+			goto('/');
+		}
 	});
 
 	const loadThumbnails = async () => {
@@ -71,10 +101,12 @@
 
 <div class="flex h-screen bg-gray-100">
 	<div
+		bind:this={thumbnailContainer}
 		class="w-48 overflow-y-auto border-r-2 border-gray-800 bg-gray-200 p-2 flex flex-col items-center gap-5"
 	>
-		{#each pages as page}
-			<div>
+		{#each pages as page (page)}
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<div class="thumbnail-sub-container">
 				<canvas
 					class="hover:cursor-pointer shadow-lg"
 					on:click={() => loadPage(page)}
