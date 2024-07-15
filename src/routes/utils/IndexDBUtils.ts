4385c5fa451845ac0fd83e81bcd591f3a9c7f2ac
Kayoshi-dev <maxime.jme@gmail.com>
2024-07-15 18:47:21 +0200

    Allow to save Blob and File aswell

--- src/routes/utils/IndexDBUtils.ts
+++ src/routes/utils/IndexDBUtils.ts
@@ -1,5 +1,5 @@
-export const saveBlob = (db: IDBDatabase, storeName: string, blob: Blob, name: string) => {
-	handleRecentFiles(db)
+export const saveBlob = (db: IDBDatabase, storeName: string, blob: Blob | File, name: string) => {
+	handleRecentFiles(db);
 	const transaction = db.transaction([storeName], 'readwrite');
 	const store = transaction.objectStore(storeName);
 	// Blob to save
@@ -21,29 +21,29 @@ export const saveBlob = (db: IDBDatabase, storeName: string, blob: Blob, name: s
 };
 
 const handleRecentFiles = (db: IDBDatabase) => {
-	const request = db.transaction('recentFiles').objectStore('recentFiles').getAll()
+	const request = db.transaction('recentFiles').objectStore('recentFiles').getAll();
 
-	request.onsuccess = ()=> {
+	request.onsuccess = () => {
 		const files = request.result;
-		if(files.length === 5) {
-			deleteOldestFile(db, files[0].id)
+		if (files.length === 5) {
+			deleteOldestFile(db, files[0].id);
 		}
-    }
+	};
 
-    request.onerror = (err)=> {
-        console.error(`Error to get files information: ${err}`)
-    }
-}
+	request.onerror = (err) => {
+		console.error(`Error to get files information: ${err}`);
+	};
+};
 
 const deleteOldestFile = (db: IDBDatabase, key: IDBValidKey) => {
-	let transaction = db.transaction(["recentFiles"], "readwrite");
-  	let request = transaction.objectStore("recentFiles").delete(key);
+	let transaction = db.transaction(['recentFiles'], 'readwrite');
+	let request = transaction.objectStore('recentFiles').delete(key);
 
-	request.onsuccess = ()=> {
-        console.log(`Oldest file deleted: ${request.result}`);
-    }
+	request.onsuccess = () => {
+		console.log(`Oldest file deleted: ${request.result}`);
+	};
 
-    request.onerror = (err)=> {
-        console.error(`Error to oldest file: ${err}`)
-    }
-}
+	request.onerror = (err) => {
+		console.error(`Error to oldest file: ${err}`);
+	};
+};
